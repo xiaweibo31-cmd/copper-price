14af29b4e2de451c94c1215d59846b8ee36dfe7f
xiaweibo31-cmd <xiaweibo31@gmail.com>
2026-06-05 08:47:19 +0800

    update data.js

--- data/data.js
+++ data/data.js
@@ -1,2 +1,31 @@
-// 铜价数据 - 等待 GitHub Actions 自动更新
-window.__COPPER_DATA = null;
+// 铜价数据 - 由 fetch.js 自动生成
+window.__COPPER_DATA = {
+  "latest": {
+    "code": "cum",
+    "name": "沪铜主连",
+    "price": 106320,
+    "change": 64,
+    "changePercent": 81,
+    "open": 106080,
+    "high": 106810,
+    "low": 105950,
+    "prevClose": 105150,
+    "volume": 93,
+    "openInterest": 116200,
+    "turnover": 25420516050,
+    "limitUp": 680,
+    "limitDown": 0,
+    "timestamp": 1780619853352,
+    "date": "2026/6/5"
+  },
+  "history": [
+    {
+      "date": "2026/6/5",
+      "price": 106320,
+      "open": 106080,
+      "high": 106810,
+      "low": 105950,
+      "volume": 93
+    }
+  ]
+};
